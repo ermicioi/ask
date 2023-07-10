d95cb39b3246236f72441512ef3f9cddf68bd1e5
Andrei Ermicioi <andrei.ermicioi@gmail.com>
2023-07-11 00:24:23 +0200

    Added menu

--- src/app/components/shared/info-panel/info-panel.module.ts
+++ src/app/components/shared/info-panel/info-panel.module.ts
@@ -2,16 +2,19 @@ import { NgModule } from '@angular/core';
 import { InfoPanelComponent } from './components/info-panel/info-panel.component';
 import { InfoPanelHeaderComponent } from './components/info-panel-header/info-panel-header.component';
 import { InfoPanelPropertiesComponent } from './components/info-panel-properties/info-panel-properties.component';
+import { InfoPanelMenuComponent } from './components/info-panel-menu/info-panel-menu.component';
 @NgModule({
   declarations: [
     InfoPanelComponent,
     InfoPanelHeaderComponent,
-    InfoPanelPropertiesComponent
+    InfoPanelPropertiesComponent,
+    InfoPanelMenuComponent
   ],
   exports: [
     InfoPanelComponent,
     InfoPanelHeaderComponent,
-    InfoPanelPropertiesComponent
+    InfoPanelPropertiesComponent,
+    InfoPanelMenuComponent
   ]
 })
 export class InfoPanelModule {
