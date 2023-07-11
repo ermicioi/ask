92f94794350ece767affd3f5294c098f0ace7c21
Andrei Ermicioi <andrei.ermicioi@gmail.com>
2023-07-12 00:37:57 +0200

    Added header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
-import {MatButtonModule} from '@angular/material/button';
-import {MatIconModule} from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
@@ -12,6 +12,8 @@ import { MenuComponent, MenuItemMarker } from './components/core/menu/menu.compo
 import { InfoPanelViewComponent } from './views/info-panel-view/info-panel-view.component';
 import { InfoPanelModule } from './components/shared/info-panel/info-panel.module';
 import { ViewBaseModule } from './components/shared/view-base/view-base.module';
+import { HeaderComponent } from './components/shared/header/components/header/header.component';
+import { HeaderViewComponent } from './views/header-view/header-view.component';
 
 const views = [
   MainViewComponent,
@@ -23,7 +25,9 @@ const views = [
     AppComponent,
     MenuComponent,
     MenuItemMarker,
-    ...views
+    ...views,
+    HeaderComponent,
+    HeaderViewComponent
   ],
   imports: [
     BrowserModule,
@@ -33,7 +37,8 @@ const views = [
     MatIconModule,
     RouterModule.forRoot([
       { path: '', component: MainViewComponent },
-      { path: 'components/info-panel', component: InfoPanelViewComponent   }
+      { path: 'components/header', component: HeaderViewComponent },
+      { path: 'components/info-panel', component: InfoPanelViewComponent }
     ]),
     ViewBaseModule,
     InfoPanelModule
