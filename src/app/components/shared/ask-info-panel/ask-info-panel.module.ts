d9d865a2ce2f5712f1bdd12119609093b9f65690
Andrei Ermicioi <andrei.ermicioi@gmail.com>
2023-07-12 22:35:55 +0200

    Renamed header component

--- src/app/components/shared/ask-info-panel/ask-info-panel.module.ts
+++ src/app/components/shared/ask-info-panel/ask-info-panel.module.ts
@@ -1,20 +1,21 @@
 import { NgModule } from '@angular/core';
 import { AskInfoPanelComponent } from './components/ask-info-panel/ask-info-panel.component';
-import { AskInfoPanelHeaderComponent } from './components/ask-info-panel-header/ask-info-panel-header.component';
 import { AskInfoPanelPropertiesComponent } from './components/ask-info-panel-properties/ask-info-panel-properties.component';
 import { AskInfoPanelMenuComponent } from './components/ask-info-panel-menu/ask-info-panel-menu.component';
+import { AskHeaderModule } from '../ask-header/ask-header.module';
 @NgModule({
   declarations: [
     AskInfoPanelComponent,
-    AskInfoPanelHeaderComponent,
     AskInfoPanelPropertiesComponent,
     AskInfoPanelMenuComponent
   ],
   exports: [
     AskInfoPanelComponent,
-    AskInfoPanelHeaderComponent,
     AskInfoPanelPropertiesComponent,
     AskInfoPanelMenuComponent
+  ],
+  imports: [
+    AskHeaderModule
   ]
 })
 export class AskInfoPanelModule {
