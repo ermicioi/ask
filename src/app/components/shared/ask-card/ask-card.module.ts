abc9a398d848276a7a3474d255db64979e1a5cb8
Andrei Ermicioi <andrei.ermicioi@gmail.com>
2023-07-15 00:00:09 +0200

    Did some improvement

--- src/app/components/shared/ask-card/ask-card.module.ts
+++ src/app/components/shared/ask-card/ask-card.module.ts
@@ -1,5 +1,9 @@
 import { NgModule } from "@angular/core";
 import { AskCardComponent } from './components/ask-card/ask-card.component';
+import { MatCardModule } from "@angular/material/card";
+import { MatIconModule } from "@angular/material/icon";
+import { BrowserModule } from "@angular/platform-browser";
+import { MatButtonModule } from "@angular/material/button";
 
 @NgModule({
   declarations: [
@@ -8,6 +12,11 @@ import { AskCardComponent } from './components/ask-card/ask-card.component';
   exports: [
     AskCardComponent
   ],
+  imports: [
+    MatButtonModule,
+    MatCardModule,
+    MatIconModule
+  ]
 })
 export class AskCardModule {
 
